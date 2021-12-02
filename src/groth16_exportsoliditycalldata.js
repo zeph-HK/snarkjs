01a88bdb7fd32e8813779955a834e43ddde99368
zeph-HK <zephyr.zk@protonmail.com>
2021-12-02 17:00:26 +0000

    Update interface for groth16 solidity export

--- src/groth16_exportsoliditycalldata.js
+++ src/groth16_exportsoliditycalldata.js
@@ -19,24 +19,30 @@
 
 function p256(n) {
     let nstr = n.toString(16);
-    while (nstr.length < 64) nstr = "0"+nstr;
-    nstr = `"0x${nstr}"`;
+    while (nstr.length < 64) nstr = "0" + nstr;
+    nstr = `${nstr}`;
     return nstr;
 }
 
 export default async function groth16ExportSolidityCallData(proof, pub) {
 
     let inputs = "";
-    for (let i=0; i<pub.length; i++) {
+    for (let i = 0; i < pub.length; i++) {
         if (inputs != "") inputs = inputs + ",";
         inputs = inputs + p256(pub[i]);
     }
 
-    let S;
-    S=`[${p256(proof.pi_a[0])}, ${p256(proof.pi_a[1])}],` +
-        `[[${p256(proof.pi_b[0][1])}, ${p256(proof.pi_b[0][0])}],[${p256(proof.pi_b[1][1])}, ${p256(proof.pi_b[1][0])}]],` +
-        `[${p256(proof.pi_c[0])}, ${p256(proof.pi_c[1])}],` +
-        `[${inputs}]`;
+    const proofHex = [
+        proof.pi_a[0],
+        proof.pi_a[1],
+        proof.pi_b[0][1],
+        proof.pi_b[0][0],
+        proof.pi_b[1][1],
+        proof.pi_b[1][0],
+        proof.pi_c[0],
+        proof.pi_c[1],
+    ].map(p256).join("");
+    const S = "0x" + proofHex + ",[" + inputs + "]";
 
     return S;
 }
